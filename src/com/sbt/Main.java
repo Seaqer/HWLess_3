6e0ad8e3cf2479e3cea5d72577b958332030b311
Tema <d@g.c>
2016-09-20 01:58:07 +0300

    Рекомендации применены

--- src/com/sbt/Main.java
+++ src/com/sbt/Main.java
@@ -3,32 +3,39 @@ package com.sbt;
 import java.util.*;
 
 public class Main {
+
+
+    private static final String TEXT = "Dynamic arrays benefit from many of the advantages of arrays, including good \n" +
+            "locality of reference and data cache utilization, compactness (low memory use), and random access. They usually have only a small fixed additional overhead for storing information about the \n" +
+            "size and capacity. This makes dynamic arrays an attractive tool for building cache-friendly data structures. However, in languages like Python or Java that enforce reference semantics, the \n" +
+            "dynamic array generally will not store the actual data, but rather it will store references to the data that resides in other areas of memory. ";
+
+
     public static void main(String[] args) {
         Scanner inConsole = new Scanner(System.in);
-        Homework myHW = new Homework();
 
         while (true) {
             System.out.println("Номер задания 1-6: ");
             switch (Integer.parseInt(inConsole.nextLine())) {
                 case 1:
-                    myHW.getAllWords();
+                    NotRepeatedWords.getAllWords(TEXT);
                     break;
                 case 2:
-                    myHW.getSortedWords();
+                    SortedWords.getSortedWords(TEXT);
                     break;
                 case 3:
-                    myHW.getNumberWords();
+                    NumberWords.getNumberWords(TEXT);
                     break;
                 case 4:
-                    myHW.getReversLines();
+                    ReversLines.getReversLines(TEXT);
                     break;
                 case 5:
-                    myHW.getIterator();
+                    MyReversLines.getIterator(TEXT);
                     break;
                 case 6:
                     System.out.println("Введите строки: ");
-                    String[] str = inConsole.nextLine().split("\\D+");
-                    myHW.showLines(str);
+                    String[] lines = inConsole.nextLine().split("\\D+");
+                    TextLines.showLines(lines,TEXT);
                     break;
                 default:
                     return;
